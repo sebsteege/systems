950ddb1e13ee064115098380c96a7c3ebf8bf5fe
sebsteege <seb.steege@gmail.com>
2025-07-03 15:35:00 -0400

    .

--- docker/docker-network.sh
+++ docker/docker-network.sh
@@ -10,3 +10,9 @@ docker network create \
   --subnet=172.20.0.16/28 \
   --gateway=172.20.0.17 \
   services_network
+
+docker network create \
+  --driver=bridge \
+  --subnet=172.20.0.32/28 \
+  --gateway=172.20.0.33 \
+  nextcloud_network
