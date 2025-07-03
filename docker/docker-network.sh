9817acea5e268902d58a96d50b0283642b2070e5
sebsteege <seb.steege@gmail.com>
2025-07-03 12:13:02 -0400

    .

--- docker/docker-network.sh
+++ docker/docker-network.sh
@@ -4,3 +4,9 @@ docker network create \
   --subnet=172.20.0.0/28 \
   --gateway=172.20.0.1 \
   media_network
+
+docker network create \
+  --driver=bridge \
+  --subnet=172.20.0.16/28 \
+  --gateway=172.20.0.17 \
+  services_network
